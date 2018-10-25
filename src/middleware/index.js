20225f37280ad45c06a5ccd46f31bb5fe5fee68d
Ahmad Tavakoli <amdtavakoli@gmail.com>
2018-10-25 00:03:34 -0600

    adding rates endpoint

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,29 @@
+// const request = require('request');
+// let request = require('async-request');
+var rp = require('request-promise');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
-  app.use('/test', (req, res) => {
-    res.json({
-      message: 'Hello world from Express middleware' + req
-    });
+  app.use('/rates', (req, res) => {
+    const baseURL = 'https://api.exchangeratesapi.io/latest?base=' + req.query.base;
+
+    var options = {
+      uri: baseURL,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      json: true,
+      gzip: true
+    };
+
+    rp(options)
+      .then(function (response) {
+        res.json(response);
+      }).catch(function (err) {
+        console.error('Error fetching latest rates', err);
+        res.json(err.error);
+      });
   });
 };
--- src/services/exchangeService.js
+++ src/services/exchangeService.js
@@ -16,7 +16,16 @@ const exchangeService = {
   async get(_id, params) {
     const {query: {exchangeRate, currency}} = params;
     const lastCalc = this.calc.Model.findOne().sort({ field: 'asc', _id: -1 }).limit(1);
-    console.log('data', lastCalc);
+    console.log('get id:', _id);
+    console.log('get params:', params);
+    lastCalc.currency = currency;
+    return this.exchangeValues(lastCalc, exchangeRate);
+  },
+
+  async find(_id, params) {
+    const {query: {exchangeRate, currency}} = params;
+    const lastCalc = this.calc.Model.findOne().sort({ field: 'asc', _id: -1 }).limit(1);
+    console.log('find');
     lastCalc.currency = currency;
     return this.exchangeValues(lastCalc, exchangeRate);
   }
